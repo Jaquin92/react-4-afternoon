a9cbcd57bc92918ac8ee159759b04d5d8fc8aa37
Jonathan <jaquin92@yahoo.com>
2018-02-06 16:01:20 -0600

    React 4 Afternoon complete

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,14 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Link, Switch, Route } from "react-router-dom";
+import History from "../History/History";
+import Contact from "../Contact/Contact";
 
 export default class About extends Component {
   render() {
     return (
       <div>
-        <div className='subnav'>
+        <div className="subnav">
+          <Link className="subnav_links" to="/about">
+            <h3>About</h3>
+          </Link>
+          <Link className="subnav_links" to="/about/history">
+            <h3>History</h3>
+          </Link>
+          <Link className="subnav_links" to="/about/contact">
+            <h3>Contact</h3>
+          </Link>
         </div>
-        <div className='box'>
+        <div className="box">
+          {" "}
+          <Switch>
+            <Route path="/about/history" component={History} />
+            <Route path="/about/contact" component={Contact} />
+            <Route
+              path="/about"
+              render={() => (
+                <div>
+                  <h1>West Side U.</h1>
+                  <p>
+                    © DevMountain LLC, 2017. Unauthorized use and/or duplication
+                    of this material without express and written permission from
+                    DevMountain, LLC is strictly prohibited. Excerpts and links
+                    may be used, provided that full and clear credit is given to
+                    DevMountain with appropriate and specific direction to the
+                    original content.
+                  </p>
+                </div>
+              )}
+            />
+          </Switch>
         </div>
       </div>
-    )
+    );
   }
 }
--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,18 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default class ClassList extends Component {
   constructor() {
-    super()
-    
+    super();
+
+    this.state = {
+      students: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(
+        `http://localhost:3005/students?class=${this.props.match.params.class}`
+      )
+      .then(results => this.setState({ students: results.data }))
+      .catch(() => console.log("error"));
   }
 
   render() {
+    const stu = this.state.students.map((item, i) => {
+      return (
+        <Link to={`/student/${item.id}`}>
+          {" "}
+          <h3 key={i}>
+            {" "}
+            {item.first_name} {item.last_name}{" "}
+          </h3>{" "}
+        </Link>
+      );
+    });
+
     return (
       <div className="box">
-        <h1></h1>
+        <h1> {this.props.match.params.class} </h1>
         <h2>ClassList:</h2>
-
+        {stu}
+        <h1>Hello</h1>
       </div>
-    )
+    );
   }
 }
